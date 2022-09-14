3bd91cd5e12d789c9f94b60ae061ddcc21e31a7c
HernanS99 <hernan.silva2299@gmail.com>
2022-09-14 12:39:18 -0300

    add card

---
+++ src/components/Catalogue.jsx
@@ -0,0 +1,16 @@
+const Catalogue = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div>
+                    <p className="fs-1 fw-bold text-center">Catalogo</p>
+                </div>
+            </div>
+            <div className="row">
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Catalogue
