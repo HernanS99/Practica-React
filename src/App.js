11cc217271f4b13c580dee7fd7c9935716b1e513
HernanS99 <hernan.silva2299@gmail.com>
2022-09-18 01:57:52 -0300

    add login view

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Home from './views/Home'
 import NotFound from './views/NotFound';
+import Login from './views/Login'
+
+
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home /> } />
       <Route path='*' element={<NotFound /> } />
+      <Route path='/Login' element={<Login/>}/>
     </Routes>
   );
 }
