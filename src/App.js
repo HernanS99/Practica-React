89d78a3d6b7a8aecdff9f57c841bd8644a470247
HernanS99 <hernan.silva2299@gmail.com>
2022-09-18 22:33:34 -0300

    add login and register

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './views/Home'
 import NotFound from './views/NotFound';
 import Register from './views/Register'
+import Login from './views/Login';
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Route path='/' element={<Home /> } />
       <Route path='*' element={<NotFound /> } />
       <Route path='/Register' element={<Register/>}/>
+      <Route path='/Login' element={<Login/>}/>
     </Routes>
   );
 }
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useEffect } from "react"
 import { useState } from "react"
 
-const Login = () => {
+const Register = () => {
     let usuarios = []
     const [user, setUser] = useState({})
 
@@ -46,8 +46,8 @@ const Login = () => {
 
                                 <div class="mb-md-5 mt-md-4 pb-5">
 
-                                    <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                                    <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                                    <h2 class="fw-bold mb-2 text-uppercase">Registrarse</h2>
+                                    <p class="text-white-50 mb-5">Ingrese los datos solicitados en la parte inferior</p>
 
                                     <div class="form-outline form-white mb-4">
                                         <input type="text" id="typeEmailX" name="firtsName" class="form-control form-control-lg"
@@ -70,23 +70,15 @@ const Login = () => {
 
                                     <div class="form-outline form-white mb-4">
                                         <input type="password" id="typePasswordX" name="password" class="form-control form-control-lg" onChange={(evento) => handleInput(evento)} />
-                                        <label class="form-label" for="typePasswordX">Password</label>
+                                        <label class="form-label" for="typePasswordX">Contraseña</label>
                                     </div>
-
-                                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
 
                                     <button class="btn btn-outline-light btn-lg px-5" type="submit" onClick={createUser}>Login</button>
-
-                                    <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                                        <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                                        <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                                        <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-                                    </div>
 
                                 </div>
 
                                 <div>
-                                    <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+                                    <p class="mb-0">ya tienes una cuenta? <a href="#!" class="text-white-50 fw-bold">Inicia sesion</a>
                                     </p>
                                 </div>
 
@@ -99,4 +91,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default Register
